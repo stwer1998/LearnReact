3b82f275665805e576ebd0b779515b246d83d262
stwer1998 <stwer1998@mail.ru>
2020-03-18 21:58:24 +0300

    add new component, export, import

--- first/src/App.js
+++ first/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Google from './Google'
 
 let App = () => {
+
   return (
+    <div>
+    
     <div className="App">
       Hello World
+    </div>
+    <Google />
     </div>
   );
 }
---
+++ first/src/Google.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+let google=()=>{
+    return <div><a href="google.ru"><p>Google</p></a></div>;
+}
+
+export default google;
