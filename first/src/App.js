fb99905030bd84e2ad011b050dc218d190a0545f
stwer1998 <stwer1998@mail.ru>
2020-03-19 17:15:47 +0300

    add new components

--- first/src/App.js
+++ first/src/App.js
@@ -1,53 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import Header from './Components/Header';
+import Navbar from './Components/Navbar';
+import Profile from './Components/Profile';
 
 let App = () => {
 
-  return (    
+  return (
     <div className='app-wrapper'>
-      <header className='header'>
-        <img src={logo} />
-      </header>
-      <nav className='nav'>
-      <div>
-            <a>Profile</a>
-        </div>
-        <div>
-            <a>Messages</a>
-        </div><div>
-            <a>News</a>
-        </div>
-        <div>
-            <a>Music</a>
-        </div>
-        <div>
-            <a>Settings</a>
-        </div>       
-      </nav>
-      <div className='content'>
-      <div>
-      <img src='https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&h=350' />
-    </div>
-    <div>
-      ava + description
-    </div>
-    <div>
-      My posts
-      <div>
-        New post
-      </div>
-      <div>
-        <div>
-          post 1
-        </div>
-        <div>
-          post 2
-        </div>
-      </div>
-    </div>
-      </div>
+      <Header />
+      <Navbar />
+      <Profile />
     </div>
   );
 }
---
+++ first/src/Components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+
+
+let Header = () => {
+    return (
+        <header className='header'>
+            <img src={logo} />
+        </header>
+    );
+}
+
+export default Header;
