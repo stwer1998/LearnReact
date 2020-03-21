5908f68262acfa020f470bb79f71fea7c8d0229a
stwer1998 <stwer1998@mail.ru>
2020-03-21 19:45:24 +0300

    add routing

--- first/src/App.js
+++ first/src/App.js
@@ -4,17 +4,20 @@ import Header from './Components/Header/Header';
 import Navbar from './Components/Navbar/Navbar';
 import Profile from './Components/Profile/Profile';
 import Dialogs from "./Components/Dialogs/Dialods";
+import {BrowserRouter,Route} from 'react-router-dom'
 
 let App = () => {
 
-  return (
-    <div className='app-wrapper'>
-      <Header />
-      <Navbar />
-      <Dialogs/>
-      {/*<Profile />*/}
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <div className='app-wrapper'>
+                <Header/>
+                <Navbar/>
+                <Route path="/dialogs" component={Dialogs}/>
+                <Route path="/profile" component={Profile}/>
+                </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
