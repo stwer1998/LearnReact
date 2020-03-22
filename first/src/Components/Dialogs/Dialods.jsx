ea92add78c1470bad84c8f1d4069f8bf041f8ff8
stwer1998 <stwer1998@mail.ru>
2020-03-22 11:09:05 +0300

    add dialogs

--- first/src/Components/Dialogs/Dialods.jsx
+++ first/src/Components/Dialogs/Dialods.jsx
@@ -2,7 +2,28 @@ import React from "react";
 import css from "./Dialogs.module.css"
 let Dialogs = () => {
     return (
-        <div className={css.content}>Dialogs</div>
+        <div className={css.dialogs}>
+            <div className={css.dialogsItem}>
+                <div className={css.dialog}>
+                    First
+                </div>
+                <div className={css.dialog+' '+css.active}>
+                    First 1
+                </div><div className={css.dialog}>
+                First 2
+            </div><div className={css.dialog}>
+                First 3
+            </div><div className={css.dialog}>
+                First 4
+            </div>
+            </div>
+            <div className={css.messages}>
+                <div className={css.message}>Hi</div>
+                <div className={css.message}>Hi yoyo</div>
+                <div className={css.message}>Hi konishua</div>
+
+            </div>
+        </div>
     );
 }
 
